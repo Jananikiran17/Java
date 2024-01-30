2ff4d5f5f81a6e497a7830f25409fe89ed470ebf
Jananikiran17 <112751883+Jananikiran17@users.noreply.github.com>
2024-01-30 18:45:05 +0530

    Add files via upload

--- Three.java
+++ Three.java
@@ -1,12 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 public class Three {
-    public static void main (String ar[]){
-
-    int a[][]={{1,2,3},{4,5,6},{7,8,9}},i,j;
-    for(i=0;i<a.length;i++){
-        for(j=0;j<a.length;j++){
-            System.out.println(a[i][j] +"");
-        }
-        System.out.println();
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7};
+        List<Integer> intList = Arrays.asList(intArray);
+        Collections.shuffle(intList);
+        intList.toArray(intArray); // This line will not resize the array
+        System.out.println(Arrays.toString(intArray));
     }
-}
 }
